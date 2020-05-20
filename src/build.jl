aa7b1294a86e1a94a364121ee369610effcb05d6
Felipe Noronha <felipenoris@gmail.com>
2020-05-20 18:37:57 -0300

    improve error message on call args check

--- src/build.jl
+++ src/build.jl
@@ -162,7 +162,7 @@ end
 
 @generated function call(op::Op, args...) :: LinearInstruction
     for a in args
-        @assert a <: AbstractValue
+        @assert a <: AbstractValue "Found argument $(a) <: $(typeof(a)) that is not an AbstractValue."
     end
 
     check_args_mutability(op, args...)
