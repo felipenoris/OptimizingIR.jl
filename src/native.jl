7ed3242b6c2b245573cc93746a67dc7731e8a551
Felipe Noronha <felipenoris@gmail.com>
2021-04-27 12:06:37 -0300

    allow native compilation out of a CompiledBasicBlock

--- src/native.jl
+++ src/native.jl
@@ -14,7 +14,9 @@ end
 
 tmpsym(i::Integer) = Symbol(:t, i)
 
-function build_function_body_expr(ir::BasicBlock)
+const BB = Union{BasicBlock, CompiledBasicBlock}
+
+function build_function_body_expr(ir::BB)
     block = Expr(:block)
 
     itr = eachinstruction(ir)
@@ -32,7 +34,7 @@ function build_function_body_expr(ir::BasicBlock)
     return block
 end
 
-function return_expr(bb::BasicBlock)
+function return_expr(bb::BB)
     out_vars = output_variables(bb)
 
     if isempty(out_vars)
@@ -58,34 +60,34 @@ function return_expr(bb::BasicBlock)
     end
 end
 
-julia_expr(bb::BasicBlock, c::CallUnary) = Expr(:call, c.op, julia_expr(bb, c.arg))
+julia_expr(bb::BB, c::CallUnary) = Expr(:call, c.op, julia_expr(bb, c.arg))
 
-function julia_expr(bb::BasicBlock, c::CallBinary)
+function julia_expr(bb::BB, c::CallBinary)
     Expr(:call, c.op, julia_expr(bb, c.arg1), julia_expr(bb, c.arg2))
 end
 
-function julia_expr(bb::BasicBlock, c::CallVararg)
+function julia_expr(bb::BB, c::CallVararg)
     Expr(:call, c.op, map( arg -> julia_expr(bb, arg), c.args)...)
 end
 
-julia_expr(bb::BasicBlock, c::ImpureInstruction) = julia_expr(bb, c.call)
-julia_expr(bb::BasicBlock, c::PureInstruction) = julia_expr(bb, c.call)
+julia_expr(bb::BB, c::ImpureInstruction) = julia_expr(bb, c.call)
+julia_expr(bb::BB, c::PureInstruction) = julia_expr(bb, c.call)
 
-julia_expr(bb::BasicBlock, constant::Const) = constant.val
-julia_expr(bb::BasicBlock, ssa::SSAValue) = tmpsym(ssa.address)
+julia_expr(bb::BB, constant::Const) = constant.val
+julia_expr(bb::BB, ssa::SSAValue) = tmpsym(ssa.address)
 
-function julia_expr(bb::BasicBlock, ::Assignment{V}) where {V<:ImmutableVariable}
+function julia_expr(bb::BB, ::Assignment{V}) where {V<:ImmutableVariable}
     # no-op
     return nothing
 end
 
-function julia_expr(bb::BasicBlock, op::Assignment{V}) where {V<:MutableVariable}
+function julia_expr(bb::BB, op::Assignment{V}) where {V<:MutableVariable}
     Expr(:(=), op.lhs.symbol, julia_expr(bb, op.rhs))
 end
 
-julia_expr(bb::BasicBlock, variable::MutableVariable) = variable.symbol
+julia_expr(bb::BB, variable::MutableVariable) = variable.symbol
 
-function julia_expr(bb::BasicBlock, variable::ImmutableVariable)
+function julia_expr(bb::BB, variable::ImmutableVariable)
     if is_input(bb, variable)
         return variable.symbol
     else
