3b95bb4f525114dff1a07c919638482250a7a540
Felipe Noronha <felipenoris@gmail.com>
2022-01-26 22:53:15 -0300

    mark lookup_table functions with inline

--- src/lookup_table.jl
+++ src/lookup_table.jl
@@ -30,41 +30,41 @@ function LookupTable(v::Vector{T}) where {T}
     return result
 end
 
-function Base.in(item::T, collection::LookupTable{T}) :: Bool where {T}
+@inline function Base.in(item::T, collection::LookupTable{T}) :: Bool where {T}
     return haskey(collection.index, item)
 end
 
-function indexof(lookup_table::LookupTable{T}, item::T) :: Int where {T}
+@inline function indexof(lookup_table::LookupTable{T}, item::T) :: Int where {T}
     return lookup_table.index[item]
 end
 
-Base.lastindex(lookup_table::LookupTable) = lastindex(lookup_table.entries)
+@inline Base.lastindex(lookup_table::LookupTable) = lastindex(lookup_table.entries)
 
-function Base.getindex(lookup_table::LookupTable{T}, key::Integer) :: T where {T}
+@inline function Base.getindex(lookup_table::LookupTable{T}, key::Integer) :: T where {T}
     return lookup_table.entries[key]
 end
 
-function Base.length(lookup_table::LookupTable{T}) where {T}
+@inline function Base.length(lookup_table::LookupTable{T}) where {T}
     return length(lookup_table.entries)
 end
 
-function Base.isempty(lookup_table::LookupTable{T}) :: Bool where {T}
+@inline function Base.isempty(lookup_table::LookupTable{T}) :: Bool where {T}
     return isempty(lookup_table.entries)
 end
 
-function Base.iterate(lookup_table::LookupTable{T}) where {T}
+@inline function Base.iterate(lookup_table::LookupTable{T}) where {T}
     return Base.iterate(lookup_table.entries)
 end
 
-function Base.iterate(lookup_table::LookupTable{T}, state) where {T}
+@inline function Base.iterate(lookup_table::LookupTable{T}, state) where {T}
     return Base.iterate(lookup_table.entries, state)
 end
 
-function Base.enumerate(lookup_table::LookupTable{T}) where {T}
+@inline function Base.enumerate(lookup_table::LookupTable{T}) where {T}
     return enumerate(lookup_table.entries)
 end
 
-function Base.filter(f, lookup_table::LookupTable{T}) where {T}
+@inline function Base.filter(f, lookup_table::LookupTable{T}) where {T}
     return filter(f, lookup_table.entries)
 end
 
