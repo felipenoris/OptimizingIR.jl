e9d8adc1adaa20cafaebb60fd778290c283178a3
Felipe Noronha <felipenoris@gmail.com>
2024-12-22 11:26:57 -0300

    update Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,11 @@ import OptimizingIR
 makedocs(
     sitename = "OptimizingIR.jl",
     modules = [ OptimizingIR ],
-    pages = [ "Home" => "index.md",
-              "API Reference" => "api.md" ]
+    pages = [
+        "Home" => "index.md",
+        "API Reference" => "api.md",
+    ],
+    checkdocs=:none,
 )
 
 deploydocs(
